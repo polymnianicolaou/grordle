73c7b8a07a0bdab06a41f891b554a3bcd4711391
polymnianicolaou <pnicola@purdue.edu>
2022-05-06 13:29:45 -0400

    added basic Grid structure, basic parsing of user input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import {Grid, TextField} from "@mui/material"
+import React, { Component } from 'react';
+const classes = {
+  root: {
+    flexGrow: 1,  
+  },
+  paper: {
+    padding: 10,
+    textAlign: "center",
+    color: "blue",
+    height: 50,
+    width: 50,
+    textTransform: "uppercase"
+
+  }
+};
+
+
+class InnerGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idx0 : '',
+      idx1 : '',
+      idx2 : '',
+      idx3 : '',
+      idx4 : ''
+     }; 
+  }
+
+  onChange = (event) => {
+    const { idx, value } = event.target;
+    this.setState({[idx] : value});
+    console.log('change', value)
+  }
+
+  //user hits enter when in the last column
+  onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Enter key pressed');
+      // write functionality here
+      //compare...
+    }
+  }
+  render() {
+      return(
+      <Grid container >
+      <TextField  style={classes.paper} idx="idx0" onChange={this.onChange}
+        inputProps={{ 
+          maxLength: 1 , 
+          style: { textTransform: "uppercase" } }} ></TextField>
+      <TextField  style={classes.paper} idx="idx1" onChange={this.onChange}
+        inputProps={{ 
+          maxLength: 1 , 
+          style: { textTransform: "uppercase" } }}></TextField>
+      <TextField  style={classes.paper} idx="idx2" onChange={this.onChange}
+        inputProps={{ 
+          maxLength: 1 , 
+          style: { textTransform: "uppercase" } }}></TextField>
+      <TextField  style={classes.paper} idx="idx3" onChange={this.onChange}
+        inputProps={{ 
+          maxLength: 1 , 
+          style: { textTransform: "uppercase" } }}></TextField>
+      <TextField  style={classes.paper} idx="idx4" onChange={this.onChange}
+        inputProps={{ 
+          maxLength: 1 , 
+          style: { textTransform: "uppercase" } }}
+          onKeyPress= {this.onKeyPress}></TextField>
+  </Grid>
+    );
+    }
+}
+
+// //the InnerGrid component will be a collection of multiple Grid items
+ function NestedGrid() {
+  return(
+    <div style={classes.root}>
+      <Grid container spacing={1} >
+          {/*Render innerGrid as a child item */}
+          <InnerGrid />
+          <InnerGrid />
+          <InnerGrid />
+          <InnerGrid />
+          <InnerGrid />
+          <InnerGrid />
+      </Grid> 
     </div>
   );
 }
 
-export default App;
+export default NestedGrid;
+
+
